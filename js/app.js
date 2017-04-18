948ff4a155cd156f718cb9408371af28085abb29
Yamila Moreno <yamila.ms@gmail.com>
2017-04-18 13:27:55 +0200

    Fixes by JF

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     // choose map providers between: https://leaflet-extras.github.io/leaflet-providers/preview/
     var outdoors = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoieWFtaWxhIiwiYSI6IjUzNDE5ZDRkZjBiZjBiZDY0YTBhZjBmNmUyZGYzYTZiIn0.okLJEzGsBQ6IOgn1mhToIQ', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                     '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                     'Imagery © <a href="http://mapbox.com">Mapbox</a>',
         id: 'mapbox.outdoors'
     });
 
@@ -14,21 +14,35 @@ $(document).ready(function() {
         center: centerMap,
         zoom: 9,
         layers: [outdoors]
+    }).on('moveend', function() {
+        drawRoutes(map, routes_bboxes);
     });
 
     var hash = new L.Hash(map);
 
-    routes_bboxes = {};
+    var routes_bboxes = {};
     for (var id in routes_dict) {
+        createGpx(routes_dict[id]).then(function(id, gpx){
+            routes_bboxes[id] = gpx;
+        }.bind(null, id));
+    }
+
+    drawRoutes(map, routes_bboxes);
+});
+
+
+function createGpx(route_dict) {
+    return new Promise(function(resolve, reject) {
         var customLink = "";
-        if (routes_dict[id].link){
-            customLink = "<br/><a href='" + routes_dict[id].link + "' target='new'>Ver historia</a>";
+        if (route_dict.link){
+            customLink = "<br/><a href='" + route_dict.link + "' target='new'>Ver historia</a>";
         }
-        customIcon = routes_dict[id].cat.icon;
-        customColor = routes_dict[id].cat.color;
+
+        var customIcon = route_dict.cat.icon;
+        var customColor = route_dict.cat.color;
 
         // Route gpx
-        gpx = new L.GPX(routes_dict[id].source, {
+        var gpx =  new L.GPX(route_dict.source, {
             max_point_interval: 7200000,
             gpx_options: {
                 parseElements: ['route', 'track']
@@ -39,7 +53,7 @@ $(document).ready(function() {
                     icon: customIcon,
                     prefix: 'ion',
                     markerColor: customColor,
-                    iconColor: 'white',
+                    iconColor: 'white'
                 }),
                 startIconUrl: null,
                 endIconUrl: null,
@@ -59,21 +73,21 @@ $(document).ready(function() {
             var distance = (e.target.get_distance() / 1000).toFixed(2);
             var content = "<i class='icon ion-" + icon + "'></i> <strong>" + name + "</strong> (" + distance + " km)" + link;
             e.target.bindPopup(content);
+            resolve(gpx);
         });
-        routes_bboxes[id] = gpx;
-    }
-
-    map.on('moveend', function() {
-        drawRoutes(map, routes_bboxes);
     });
-    drawRoutes(map, routes_bboxes);
-});
+}
 
 function drawRoutes(map, routes_bboxes) {
     var bboxMap = map.getBounds();
     for (var id in routes_bboxes) {
         if (bboxMap.intersects(routes_bboxes[id].getBounds())) {
+            console.log("SÍ!");
+            console.log(routes_bboxes[id].get_name());
             routes_bboxes[id].addTo(map);
+        } else {
+            console.log("NO!");
+            console.log(routes_bboxes[id].get_name());
         }
     }
 }
