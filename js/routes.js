6c64eb42ac2f673d81a37208fb1de985f66fe633
Luis GC <luisgc@gmail.com>
2019-12-28 21:14:52 +0100

    2018-05-03-Corrieshalloch-Gorge

--- js/routes.js
+++ js/routes.js
@@ -10,6 +10,10 @@ var routes_dict = {
         "cat": biking_cat
     },
 // 2018
+    "2018-05-03-Corrieshalloch-Gorge": {
+        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-03-Corrieshalloch-Gorge.gpx",
+        "cat": hiking_cat
+    },
     "2018-05-03-Rogie-Falls": {
         "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-03-Rogie-Falls.gpx",
         "cat": hiking_cat
