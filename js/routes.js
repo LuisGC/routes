72cb1a6a75730b5fbdb588105aaaa24dec63a526
Luis GC <luisgc@gmail.com>
2019-12-28 20:14:59 +0100

    VIII vinos con historia added

--- js/routes.js
+++ js/routes.js
@@ -1,12 +1,18 @@
 var routes_dict = {
-    // HIKING
+// 2012
     "Loire-a-Velo": {
         "source": "https://raw.githubusercontent.com/yamila-moreno/routes/gh-pages/gpx/cycling/2012-09_Loire-a-Velo.gpx",
         "cat": biking_cat
     },
+// 2016
     "vv-ojos-negros": {
         "source": "https://raw.githubusercontent.com/yamila-moreno/routes/gh-pages/gpx/cycling/2016-05-17_Via_Verde_Ojos_Negros.gpx",
-        "link": "https://dendarii.es/2016/08/via-verde-de-ojos-negros/",
+        "cat": biking_cat
+    },
+// 2019
+    "viii-vinos-con-historia": {
+        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-06-16-Ruta-VIII-Vinos-con-Historia.gpx",
         "cat": biking_cat
     }
+
 };
