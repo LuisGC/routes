4858ba7944fd381bf18276093d7870b1cd763bcc
Luis GC <luisgc@gmail.com>
2019-12-28 21:44:32 +0100

    format fixed

--- js/routes.js
+++ js/routes.js
@@ -46,88 +46,88 @@ var routes_dict = {
     },
 // 2018
     "2018-03-10-Rio-Dulce": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2018-03-10-Rio-Dulce.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2018-03-10-Rio-Dulce.gpx",
+      "cat": hiking_cat
     },
     "2018-04-30-Puck's-Glen": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-04-30-Puck's-Glen.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-04-30-Puck's-Glen.gpx",
+      "cat": hiking_cat
     },
     "2018-05-01-Eagle's-Eyrie-Benderloch": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-01-Eagle's-Eyrie-Benderloch.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-01-Eagle's-Eyrie-Benderloch.gpx",
+      "cat": hiking_cat
     },
     "2018-05-01-Lost-Valley": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-01-Lost-Valley.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-01-Lost-Valley.gpx",
+      "cat": hiking_cat
     },
     "2018-05-02-Old-Man-of-Storr": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-02-Old-Man-of-Storr.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-02-Old-Man-of-Storr.gpx",
+      "cat": hiking_cat
     },
     "2018-05-02-Quiraing": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-02-Quiraing.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-02-Quiraing.gpx",
+      "cat": hiking_cat
     },
     "2018-05-02-Fairy-Glen": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-02-Fairy-Glen.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-02-Fairy-Glen.gpx",
+      "cat": hiking_cat
     },
     "2018-05-02-Neist-Point": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-02-Neist-Point.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-02-Neist-Point.gpx",
+      "cat": hiking_cat
     },
     "2018-05-03-Corrieshalloch-Gorge": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-03-Corrieshalloch-Gorge.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-03-Corrieshalloch-Gorge.gpx",
+      "cat": hiking_cat
     },
     "2018-05-03-Rogie-Falls": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-03-Rogie-Falls.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-03-Rogie-Falls.gpx",
+      "cat": hiking_cat
     },
     "2018-05-04-Loch-an-Eilein": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-04-Loch-an-Eilein.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-04-Loch-an-Eilein.gpx",
+      "cat": hiking_cat
     },
     "2018-05-04-Falls-of-Bruar": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-04-Falls-of-Bruar.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-04-Falls-of-Bruar.gpx",
+      "cat": hiking_cat
     },
     "2018-08-14-Fervenza-do-Toxa": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2018-08-14-Fervenza-do-Toxa.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2018-08-14-Fervenza-do-Toxa.gpx",
+      "cat": hiking_cat
     },
     "2018-08-17-Isla-de-Ons": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2018-08-17-Isla-de-Ons.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2018-08-17-Isla-de-Ons.gpx",
+      "cat": hiking_cat
     },
     "2018-09-08-Romeria-a-Mamblas": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2018-09-08-Romeria-a-Mamblas.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2018-09-08-Romeria-a-Mamblas.gpx",
+      "cat": hiking_cat
     },
     "2018-09-09-Romeria-a-Redonda": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2018-09-09-Romeria-a-Redonda.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2018-09-09-Romeria-a-Redonda.gpx",
+      "cat": hiking_cat
     },
 // 2019
     "2019-01-13-La-Jarosa": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-01-13-La-Jarosa.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-01-13-La-Jarosa.gpx",
+      "cat": hiking_cat
     },
     "2019-03-16-Navarredonda-de-Gredos": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-03-16-Navarredonda-de-Gredos.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-03-16-Navarredonda-de-Gredos.gpx",
+      "cat": hiking_cat
     },
     "2019-06-16-Ruta-VIII-Vinos-con-Historia": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-06-16-Ruta-VIII-Vinos-con-Historia.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-06-16-Ruta-VIII-Vinos-con-Historia.gpx",
+      "cat": hiking_cat
     },
     "2019-06-16-Jiankou-to-Mutianyu-Great-Wall-Hike": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/china/2019-06-16-Jiankou-to-Mutianyu-Great-Wall-Hike.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/china/2019-06-16-Jiankou-to-Mutianyu-Great-Wall-Hike.gpx",
+      "cat": hiking_cat
     },
     "2019-11-30-Fuenfria-desde-Cercedilla": {
-        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-11-30-Fuenfria-desde-Cercedilla.gpx",
-        "cat": hiking_cat
+      "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-11-30-Fuenfria-desde-Cercedilla.gpx",
+      "cat": hiking_cat
     }
 };
