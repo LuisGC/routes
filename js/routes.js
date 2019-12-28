bf2ade4a31ab48954396e2677e69d812c89c8e30
Luis GC <luisgc@gmail.com>
2019-12-28 21:19:47 +0100

    2018-05-02-Quiraing

--- js/routes.js
+++ js/routes.js
@@ -10,6 +10,10 @@ var routes_dict = {
         "cat": biking_cat
     },
 // 2018
+    "2018-05-02-Quiraing": {
+        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-02-Quiraing.gpx",
+        "cat": hiking_cat
+    },
     "2018-05-02-Fairy-Glen": {
         "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/scotland/2018-05-02-Fairy-Glen.gpx",
         "cat": hiking_cat
