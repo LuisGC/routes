ea7ed8268e0fd4dec585cb6b35cd8363354b2f70
Luis GC <luisgc@gmail.com>
2019-12-29 00:44:23 +0100

    2012-07-15-Valle-Rio-Dulce-Siguenza

--- js/routes.js
+++ js/routes.js
@@ -9,6 +9,10 @@ var routes_dict = {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2012-02-18-La-Pedriza.gpx",
     "cat": hiking_cat
   },
+  "2012-07-15-Valle-Rio-Dulce-Siguenza": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2012-07-15-Valle-Rio-Dulce-Siguenza.gpx",
+    "cat": biking_cat
+  },
   "2012-09_Loire-a-Velo": {
     "source": "https://raw.githubusercontent.com/yamila-moreno/routes/gh-pages/gpx/cycling/2012-09_Loire-a-Velo.gpx",
     "cat": biking_cat
