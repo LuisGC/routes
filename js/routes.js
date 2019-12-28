0f39d9b3a7f17f365d7e0d6ae59c92248eb2f418
Luis GC <luisgc@gmail.com>
2019-12-28 20:52:38 +0100

    2019-03-16-Navarredonda-de-Gredos added

--- js/routes.js
+++ js/routes.js
@@ -1,24 +1,28 @@
 var routes_dict = {
 // 2012
-    "Loire-a-Velo": {
+    "2012-09_Loire-a-Velo": {
         "source": "https://raw.githubusercontent.com/yamila-moreno/routes/gh-pages/gpx/cycling/2012-09_Loire-a-Velo.gpx",
         "cat": biking_cat
     },
 // 2016
-    "vv-ojos-negros": {
+    "2016-05-17-Via-Verde-Ojos-Negros": {
         "source": "https://raw.githubusercontent.com/yamila-moreno/routes/gh-pages/gpx/cycling/2016-05-17_Via_Verde_Ojos_Negros.gpx",
         "cat": biking_cat
     },
 // 2019
-    "viii-vinos-con-historia": {
+    "2019-03-16-Navarredonda-de-Gredos": {
+        "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-03-16-Navarredonda-de-Gredos.gpx",
+        "cat": hiking_cat
+    },
+    "2019-06-16-Ruta-VIII-Vinos-con-Historia": {
         "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-06-16-Ruta-VIII-Vinos-con-Historia.gpx",
         "cat": hiking_cat
     },
-    "Jiankou-to-Mutianyu-Great-Wall-Hike": {
+    "2019-06-16-Jiankou-to-Mutianyu-Great-Wall-Hike": {
         "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/china/2019-06-16-Jiankou-to-Mutianyu-Great-Wall-Hike.gpx",
         "cat": hiking_cat
     },
-    "Fuenfria-desde-cercedilla": {
+    "2019-11-30-Fuenfria-desde-Cercedilla": {
         "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-11-30-Fuenfria-desde-Cercedilla.gpx",
         "cat": hiking_cat
     }
