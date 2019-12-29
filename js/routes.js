91745a6aaa0598d06ae62bbf5c7367338ad6fb70
Luis GC <luisgc@gmail.com>
2019-12-29 01:04:32 +0100

    2012-08-13-Carabanchel

--- js/routes.js
+++ js/routes.js
@@ -41,6 +41,10 @@ var routes_dict = {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2012-08-05-Via-Verde-Tajuna.gpx",
     "cat": biking_cat
   },
+  "2012-08-13-Carabanchel": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2012-08-13-Carabanchel.gpx",
+    "cat": biking_cat
+  },
   "2012-09_Loire-a-Velo": {
     "source": "https://raw.githubusercontent.com/yamila-moreno/routes/gh-pages/gpx/cycling/2012-09_Loire-a-Velo.gpx",
     "cat": biking_cat
