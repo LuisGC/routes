f2a0d4166633753a4bf0e62d879b843c49f62e5c
Luis GC <luisgc@gmail.com>
2020-02-29 20:43:51 +0100

    2018-10-28-Stonehenge added

--- js/routes.js
+++ js/routes.js
@@ -367,6 +367,10 @@ var routes_dict = {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2018-10-06-Navegando-Altea.gpx",
     "cat": boat_cat
   },
+  "2018-10-28-Stonehenge": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/england/2018-10-28-Stonehenge.gpx",
+    "cat": walking_cat
+  },
   "2018-11-16-Walking-in-Prague": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/czech-republic/2018-11-16-Walking-in-Prague.gpx",
     "cat": walking_cat
