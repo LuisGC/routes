431f1b3c08dea0798838a1344b305e05b311eefc
Luis GC <luisgc@gmail.com>
2020-08-31 13:12:43 +0200

    2018-12-05-Romantische-Straße-14 added

--- js/routes.js
+++ js/routes.js
@@ -447,6 +447,10 @@ var routes_dict = {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/germany/2018-12-05-Romantische-Straße-13.gpx",
     "cat": car_cat
   },
+  "2018-12-05-Romantische-Straße-14": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/germany/2018-12-05-Romantische-Straße-14.gpx",
+    "cat": car_cat
+  },
   // 2019
   "2019-01-13-La-Jarosa": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-01-13-La-Jarosa.gpx",
