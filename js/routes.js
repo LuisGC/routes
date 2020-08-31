ac9c3f6b74c58a8b4e215b7b01fcba02146f9035
Luis GC <luisgc@gmail.com>
2020-08-31 12:35:05 +0200

    2018-12-02 Romantische Straße 5 added

--- js/routes.js
+++ js/routes.js
@@ -411,6 +411,10 @@ var routes_dict = {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/germany/2018-12-02-Romantische-Straße-4.gpx",
     "cat": car_cat
   },
+  "2018-12-02-Romantische-Straße-5": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/germany/2018-12-02-Romantische-Straße-5.gpx",
+    "cat": walking_cat
+  },
   // 2019
   "2019-01-13-La-Jarosa": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2019-01-13-La-Jarosa.gpx",
