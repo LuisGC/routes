6f90d285ea4e0a6f6c0bbcc2ba446134bc4de4f8
Luis GC <luisgc@gmail.com>
2020-11-02 20:14:19 +0100

    2016-08-31-Wroklaw added

--- js/routes.js
+++ js/routes.js
@@ -381,6 +381,10 @@ var routes_dict = {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/poland/2016-08-30-Zakopane.gpx",
     "cat": walking_cat
   },
+  "2016-08-31-Wroklaw": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/poland/2016-08-31-Wroklaw.gpx",
+    "cat": walking_cat
+  },
   "2016-10-09-Puentes-Medievales-Lozoya": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2016-10-09-Puentes-Medievales-Lozoya.gpx",
     "cat": hiking_cat
