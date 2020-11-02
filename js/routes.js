14c0f472a419da414b64517032158fd58bc6f174
Luis GC <luisgc@gmail.com>
2020-11-02 13:04:13 +0100

    2014-08-13-Ruta-Bodegas-Covarrubias added

--- js/routes.js
+++ js/routes.js
@@ -267,6 +267,10 @@ var routes_dict = {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2014-08-03-Sanchinarro-El-Goloso.gpx",
     "cat": biking_cat
   },
+  "2014-08-13-Ruta-Bodegas-Covarrubias": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2014-08-13-Ruta-Bodegas-Covarrubias.gpx",
+    "cat": hiking_cat
+  },
   "2014-08-15-Valle-medio-del-Arlanza": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2014-08-15-Valle-medio-del-Arlanza.gpx",
     "cat": hiking_cat
