595ae44c9d7c33fb7d1a240196ed559d7c6951b6
Luis GC <luisgc@gmail.com>
2020-11-02 19:55:06 +0100

    2016-08-26-Nowa-Huta added

--- js/routes.js
+++ js/routes.js
@@ -353,6 +353,10 @@ var routes_dict = {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/poland/2016-08-25-Krakow.gpx",
     "cat": walking_cat
   },
+  "2016-08-26-Nowa-Huta": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/poland/2016-08-26-Nowa-Huta.gpx",
+    "cat": walking_cat
+  },
   "2016-10-09-Puentes-Medievales-Lozoya": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2016-10-09-Puentes-Medievales-Lozoya.gpx",
     "cat": hiking_cat
