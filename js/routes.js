2a86621edae26de3b93214122936427f0aa751de
Luis GC <luisgc@gmail.com>
2020-11-02 20:36:05 +0100

    2016-09-06-Krakow added

--- js/routes.js
+++ js/routes.js
@@ -421,6 +421,10 @@ var routes_dict = {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/poland/2016-09-05-Wieliczka.gpx",
     "cat": walking_cat
   },
+  "2016-09-06-Krakow": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/poland/2016-09-06-Krakow.gpx",
+    "cat": walking_cat
+  },
   "2016-10-09-Puentes-Medievales-Lozoya": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2016-10-09-Puentes-Medievales-Lozoya.gpx",
     "cat": hiking_cat
