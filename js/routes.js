7d2d4d126f52691c5489d270446b78670b0a5966
Luis GC <luisgc@gmail.com>
2021-03-24 19:16:19 +0100

    2021-03-21-Cercedilla-La-Penota

--- js/routes.js
+++ js/routes.js
@@ -736,5 +736,9 @@ var routes_dict = {
   "2021-03-20-Tranco-Pedriza-Cantocochino": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2021-03-20-Tranco-Pedriza-Cantocochino.gpx",
     "cat": hiking_cat
+  },
+  "2021-03-21-Cercedilla-La-Penota": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2021-03-21-Cercedilla-La-Penota.gpx",
+    "cat": hiking_cat
   }
 };
