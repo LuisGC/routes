05c5ec7a031c30bde005aafadcffc0bb3572de25
Luis GC <luisgc@gmail.com>
2021-11-27 15:47:20 +0100

    2021-11-08 Parque Natural Señorío de Bertiz

--- js/routes.js
+++ js/routes.js
@@ -796,5 +796,9 @@ var routes_dict = {
   "2021-11-06-Selva-Irati-Sendero-paraísos-Erlan-Pantano-Irabia": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2021/2021-11-06-Selva-Irati-Sendero-paraísos-Erlan-Pantano-Irabia.gpx",
     "cat": hiking_cat
+  },
+  "2021-11-08-Parque-Natural-Senorio-Bertiz": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2021/2021-11-08-Parque-Natural-Senorio-Bertiz.gpx",
+    "cat": hiking_cat
   }
 };
