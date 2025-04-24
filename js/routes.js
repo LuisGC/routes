1b2ab2169c2b149d5a6ba653053e67c7b256cda3
Luis GC <luisgc@gmail.com>
2025-04-24 20:32:10 +0200

    2024-09-06 Hengifoss y Litlanesfoss

--- js/routes.js
+++ js/routes.js
@@ -897,6 +897,10 @@ var routes_dict = {
     "cat": archery_cat
   },
   // 2024
+  "2024-09-06 Hengifoss y Litlanesfoss": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/iceland/2024-09-06_Hengifoss-Litlanesfoss.gpx",
+    "cat": hiking_cat
+  },
   "2024-09-07 Rjúkandafoss": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/iceland/2024-09-07_Rjukandafoss.gpx",
     "cat": hiking_cat
