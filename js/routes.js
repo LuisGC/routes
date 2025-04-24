8df5d3a78d13e8f442b90db0fa1a694831989976
Luis GC <luisgc@gmail.com>
2025-04-24 20:40:35 +0200

    2024-09-04 Fjadrárgljúfur

--- js/routes.js
+++ js/routes.js
@@ -897,6 +897,10 @@ var routes_dict = {
     "cat": archery_cat
   },
   // 2024
+  "2024-09-04 Fjadrárgljúfur": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/iceland/2024-09-04_Fjadrargljufur.gpx",
+    "cat": hiking_cat
+  },
   "2024-09-04 Hundafoss y Svartifoss": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/iceland/2024-09-04_Hundafoss-Svartifoss.gpx",
     "cat": hiking_cat
