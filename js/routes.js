5636d16d18024280dd48cc1305584db67f8c5d40
Luis GC <luisgc@gmail.com>
2025-04-24 20:28:03 +0200

    2024-09-07 Selfoss y Dettifoss

--- js/routes.js
+++ js/routes.js
@@ -897,6 +897,10 @@ var routes_dict = {
     "cat": archery_cat
   },
   // 2024
+  "2024-09-07 Selfoss y Dettifoss": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/iceland/2024-09-07_Selfoss-Dettifoss.gpx",
+    "cat": hiking_cat
+  },
   "2024-09-07 Hljódaklettar": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/iceland/2024-09-07_Hljodaklettar.gpx",
     "cat": hiking_cat
