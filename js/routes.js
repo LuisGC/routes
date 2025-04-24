a65f01909315d2b917352adecf03bc872c7b9ab3
Luis GC <luisgc@gmail.com>
2025-04-24 20:25:10 +0200

    2024-09-08 Hverir

--- js/routes.js
+++ js/routes.js
@@ -897,6 +897,10 @@ var routes_dict = {
     "cat": archery_cat
   },
   // 2024
+  "2024-09-08 Hverir": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/iceland/2024-09-08_Hverir.gpx",
+    "cat": hiking_cat
+  },
   "2024-09-08 Leirhnjúkur": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/iceland/2024-09-08_Leirhnjukur.gpx",
     "cat": hiking_cat
