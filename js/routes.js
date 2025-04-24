3a011fb458d44855d1ddd35b4f0b43fa8a2947b1
Luis GC <luisgc@gmail.com>
2025-04-24 20:33:45 +0200

    2024-09-05 Fjallsjökull

--- js/routes.js
+++ js/routes.js
@@ -897,6 +897,10 @@ var routes_dict = {
     "cat": archery_cat
   },
   // 2024
+  "2024-09-05 Fjallsjökull": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/iceland/2024-09-05_Fjallsjokull.gpx",
+    "cat": hiking_cat
+  },
   "2024-09-06 Hengifoss y Litlanesfoss": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/iceland/2024-09-06_Hengifoss-Litlanesfoss.gpx",
     "cat": hiking_cat
