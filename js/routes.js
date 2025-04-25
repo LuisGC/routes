3183d22d16e19ad8f63edbdc392dc37d104025f2
Luis GC <luisgc@gmail.com>
2025-04-25 10:48:59 +0200

    2024-08-15 Fervenza do Santo Estevo do Ermo y Mirador de Penabor

--- js/routes.js
+++ js/routes.js
@@ -897,6 +897,10 @@ var routes_dict = {
     "cat": archery_cat
   },
   // 2024
+  "2024-08-15 Fervenza do Santo Estevo do Ermo y Mirador de Penabor": {
+    "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2024/2024-08-15_Fervenza-do-Santo-Estevo-do-Ermo.gpx",
+    "cat": hiking_cat
+  },
   "2024-08-16 Paseo dos namorados y Senda Botánica de Sargadelos": {
     "source": "https://raw.githubusercontent.com/LuisGC/routes/master/gpx/spain/2024/2024-08-16_Paseo-dos-Namorados-Sargadelos.gpx",
     "cat": hiking_cat
